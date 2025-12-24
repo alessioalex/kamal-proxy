9149527141c6d2e94b1a20511dd69e323cc477f3
Alexandru Vlăduţu <github@social.alessioalex.com>
2025-12-24 23:35:08 +0200

    Added websockets and server sent events to example

---
+++ example/upstream/go.mod
@@ -0,0 +1,5 @@
+module github.com/basecamp/kamal-proxy-example
+
+go 1.25.5
+
+require github.com/gorilla/websocket v1.5.3
--- example/upstream/main.go
+++ example/upstream/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"cmp"
+	_ "embed"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
 )
+
+//go:embed chat.html
+var chatHTML []byte
 
 func upHandler(w http.ResponseWriter, r *http.Request) {
 	slog.Info("Health request", "method", r.Method, "url", r.URL)
@@ -19,13 +23,71 @@ func helloHandler(host string) http.HandlerFunc {
 		host = cmp.Or(r.Header.Get("X-Kamal-Target"), host)
 
 		w.Header().Add("Content-Type", "text/html")
-		fmt.Fprintf(w, "<body>Hello from <strong>%s</strong> at <strong>%s</strong></body>\n",
+		fmt.Fprintf(w, "<p>Hello from <strong>%s</strong> at <strong>%s</strong></p>\n",
 			host,
 			time.Now().Format(time.RFC3339),
 		)
 
+		script := `
+			<p>Server sent events list:</p>
+
+			<script>
+				const evtSource = new EventSource("/sse");
+
+				const eventList = document.createElement("ul");
+				document.body.appendChild(eventList);
+
+				evtSource.onmessage = (event) => {
+					const newElement = document.createElement("li");
+					newElement.textContent = "message: " + event.data;
+					eventList.appendChild(newElement);
+				};
+			</script>
+		`
+		fmt.Fprintf(w, script)
+
 		slog.Info("Request", "host", host, "request_id", r.Header.Get("X-Request-ID"), "method", r.Method, "url", r.URL)
 	}
+}
+
+func sseHandler(w http.ResponseWriter, r *http.Request) {
+	// Set headers to mimic SSE
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("X-Accel-Buffering", "no")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	w.Write([]byte("data: hello\n\n"))
+
+	rc := http.NewResponseController(w)
+	if err := rc.Flush(); err != nil {
+		fmt.Println("Couldn't flush!")
+		fmt.Println(err)
+	}
+
+	time.Sleep(4 * time.Second)
+	w.Write([]byte("data: world!!!\n\n"))
+	if err := rc.Flush(); err != nil {
+		fmt.Println("Couldn't flush!")
+		fmt.Println(err)
+	}
+
+	time.Sleep(40 * time.Second)
+	w.Write([]byte("data: the end!!!\n\n"))
+}
+
+func serveChat(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	slog.Info("Serve /chat request", "method", r.Method, "url", r.URL)
+	w.Write(chatHTML)
 }
 
 func main() {
@@ -34,8 +96,15 @@ func main() {
 		panic(err)
 	}
 
-	http.HandleFunc("/up", upHandler)
-	http.HandleFunc("/", helloHandler(host))
+	hub := newHub()
+	go hub.run()
 
+	http.HandleFunc("/", serveChat)
+	http.HandleFunc("/up", upHandler)
+	http.HandleFunc("/test-sse", helloHandler(host))
+	http.HandleFunc("/sse", sseHandler)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
 	panic(http.ListenAndServe(":80", nil))
 }
